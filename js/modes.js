f29f74b7936b6ebabef898771bf6b54bffc2227d
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-01-08 15:29:55 +0100

    Toggle optimization

--- js/modes.js
+++ js/modes.js
@@ -1,7 +1,23 @@
-// Mode button switch and replace
 const toggleModeButton = document.getElementById("toggle-mode");
 const bodyElement = document.querySelector("body");
 const linkElement = document.querySelector('link[rel="stylesheet"]');
+
+// Check for saved mode in localStorage and apply it on page load
+const currentMode = localStorage.getItem("mode");
+if (currentMode === "dark") {
+  bodyElement.classList.add("dark-mode");
+  linkElement.setAttribute("href", "../styles/dark-mode.css");
+  toggleModeButton.innerHTML = "Light Mode";
+} else if (currentMode === "light") {
+  bodyElement.classList.add("light-mode");
+  linkElement.setAttribute("href", "../styles/light-mode.css");
+  toggleModeButton.innerHTML = "Dark Mode";
+} else {
+  // By default, make it dark mode
+  bodyElement.classList.add("dark-mode");
+  linkElement.setAttribute("href", "../styles/dark-mode.css");
+  toggleModeButton.innerHTML = "Light Mode";
+}
 
 function toggleMode() {
   if (bodyElement.classList.contains("light-mode")) {
@@ -18,11 +34,3 @@ function toggleMode() {
 }
 
 toggleModeButton.addEventListener("click", toggleMode);
-
-// Check for saved mode in localStorage and apply it on page load
-const currentMode = localStorage.getItem("mode");
-if (currentMode === "dark") {
-  bodyElement.classList.replace("light-mode", "dark-mode");
-  linkElement.setAttribute("href", "../styles/dark-mode.css");
-  toggleModeButton.innerHTML = "Light Mode";
-}
