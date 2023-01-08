3fbcc0d674e137cd24f4c15369cdc7825b6264f7
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-01-08 03:52:05 +0100

    Applying modes to all pages

--- js/modes.js
+++ js/modes.js
@@ -8,18 +8,18 @@ function toggleMode() {
     bodyElement.classList.replace("light-mode", "dark-mode");
     linkElement.setAttribute("href", "../styles/dark-mode.css");
     toggleModeButton.innerHTML = "Light Mode";
-    localStorage.setItem("mode", "dark"); // set mode to dark in local storage
+    localStorage.setItem("mode", "dark"); // Set mode to dark in local storage
   } else {
     bodyElement.classList.replace("dark-mode", "light-mode");
     linkElement.setAttribute("href", "../styles/light-mode.css");
     toggleModeButton.innerHTML = "Dark Mode";
-    localStorage.setItem("mode", "light"); // set mode to light in local storage
+    localStorage.setItem("mode", "light"); // Set mode to light in local storage
   }
 }
 
 toggleModeButton.addEventListener("click", toggleMode);
 
-// Check for saved mode in local storage and apply it on page load
+// Check for saved mode in localStorage and apply it on page load
 const currentMode = localStorage.getItem("mode");
 if (currentMode === "dark") {
   bodyElement.classList.replace("light-mode", "dark-mode");
