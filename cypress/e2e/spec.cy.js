c5574607837a8084de228b0c1a34422b6b3a254a
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-01-10 09:08:54 +0100

    Cypress testing // tweet button

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,4 @@
+let numTabs;
 describe("Full test", () => {
   beforeEach(() => {
     cy.visit("https://philosophyinspo.000webhostapp.com/");
@@ -28,63 +29,155 @@ describe("Full test", () => {
   });
   it("checks if the generate quote button works", () => {
     cy.visit("https://philosophyinspo.000webhostapp.com/pages/classicalgreek.html");
-    cy.wait(2000);
+    cy.wait(3000);
     cy.get("#quote").then(($div) => {
       const initialText = $div.text();
       cy.get("#new-quote").click();
-      cy.wait(2000);
+      cy.wait(3000);
       cy.get("#quote").should(($div) => {
         expect($div.text()).not.eq(initialText);
       });
     });
     cy.visit("https://philosophyinspo.000webhostapp.com/pages/empiricism.html");
-    cy.wait(2000);
+    cy.wait(3000);
     cy.get("#quote").then(($div) => {
       const initialText = $div.text();
       cy.get("#new-quote").click();
-      cy.wait(2000);
+      cy.wait(3000);
       cy.get("#quote").should(($div) => {
         expect($div.text()).not.eq(initialText);
       });
     });
     cy.visit("https://philosophyinspo.000webhostapp.com/pages/existentialism.html");
-    cy.wait(2000);
+    cy.wait(3000);
     cy.get("#quote").then(($div) => {
       const initialText = $div.text();
       cy.get("#new-quote").click();
-      cy.wait(2000);
+      cy.wait(3000);
       cy.get("#quote").should(($div) => {
         expect($div.text()).not.eq(initialText);
       });
     });
     cy.visit("https://philosophyinspo.000webhostapp.com/pages/mysticism.html");
-    cy.wait(2000);
+    cy.wait(3000);
     cy.get("#quote").then(($div) => {
       const initialText = $div.text();
       cy.get("#new-quote").click();
-      cy.wait(2000);
+      cy.wait(3000);
       cy.get("#quote").should(($div) => {
         expect($div.text()).not.eq(initialText);
       });
     });
     cy.visit("https://philosophyinspo.000webhostapp.com/pages/rationalism.html");
-    cy.wait(2000);
+    cy.wait(3000);
     cy.get("#quote").then(($div) => {
       const initialText = $div.text();
       cy.get("#new-quote").click();
-      cy.wait(2000);
+      cy.wait(3000);
       cy.get("#quote").should(($div) => {
         expect($div.text()).not.eq(initialText);
       });
     });
     cy.visit("https://philosophyinspo.000webhostapp.com/pages/stoicism.html");
-    cy.wait(2000);
+    cy.wait(3000);
     cy.get("#quote").then(($div) => {
       const initialText = $div.text();
       cy.get("#new-quote").click();
-      cy.wait(2000);
+      cy.wait(3000);
       cy.get("#quote").should(($div) => {
         expect($div.text()).not.eq(initialText);
+      });
+    });
+  });
+  it("checks if the tweet button works", () => {
+    cy.visit("https://philosophyinspo.000webhostapp.com/pages/classicalgreek.html");
+    cy.window().then((win) => {
+      numTabs = win.length;
+    });
+
+    // Click the button
+    cy.get("#twitter").click();
+
+    // Listen for the tab:changed event
+    cy.on("tab:changed", () => {
+      // Get the number of tabs after clicking the button
+      cy.window().then((win) => {
+        expect(win.length).to.eq(numTabs + 1);
+      });
+    });
+    cy.visit("https://philosophyinspo.000webhostapp.com/pages/empiricism.html");
+    cy.window().then((win) => {
+      numTabs = win.length;
+    });
+
+    // Click the button
+    cy.get("#twitter").click();
+
+    // Listen for the tab:changed event
+    cy.on("tab:changed", () => {
+      // Get the number of tabs after clicking the button
+      cy.window().then((win) => {
+        expect(win.length).to.eq(numTabs + 1);
+      });
+    });
+    cy.visit("https://philosophyinspo.000webhostapp.com/pages/existentialism.html");
+    cy.window().then((win) => {
+      numTabs = win.length;
+    });
+
+    // Click the button
+    cy.get("#twitter").click();
+
+    // Listen for the tab:changed event
+    cy.on("tab:changed", () => {
+      // Get the number of tabs after clicking the button
+      cy.window().then((win) => {
+        expect(win.length).to.eq(numTabs + 1);
+      });
+    });
+    cy.visit("https://philosophyinspo.000webhostapp.com/pages/mysticism.html");
+    cy.window().then((win) => {
+      numTabs = win.length;
+    });
+
+    // Click the button
+    cy.get("#twitter").click();
+
+    // Listen for the tab:changed event
+    cy.on("tab:changed", () => {
+      // Get the number of tabs after clicking the button
+      cy.window().then((win) => {
+        expect(win.length).to.eq(numTabs + 1);
+      });
+    });
+    cy.visit("https://philosophyinspo.000webhostapp.com/pages/rationalism.html");
+    cy.window().then((win) => {
+      numTabs = win.length;
+    });
+
+    // Click the button
+    cy.get("#twitter").click();
+
+    // Listen for the tab:changed event
+    cy.on("tab:changed", () => {
+      // Get the number of tabs after clicking the button
+      cy.window().then((win) => {
+        expect(win.length).to.eq(numTabs + 1);
+      });
+    });
+    cy.visit("https://philosophyinspo.000webhostapp.com/pages/stoicism.html");
+    cy.window().then((win) => {
+      numTabs = win.length;
+    });
+
+    // Click the button
+    cy.get("#twitter").click();
+
+    // Listen for the tab:changed event
+    cy.on("tab:changed", () => {
+      // Get the number of tabs after clicking the button
+      cy.window().then((win) => {
+        expect(win.length).to.eq(numTabs + 1);
       });
     });
   });
